65179d631d06b83cace7d60aee96a2a35631bb8c
bonganimokase <99387235+bonganimokase@users.noreply.github.com>
2022-02-14 23:08:14 +0200

    add an operating system user mob_app_usr
    #2Modify setup.sh to create a user
    Modify the setup.sh script to add an operating system user mob_app_usr 
    The script should install a home directory for the user

---
+++ backend/sprint1/setup.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+#Modify setup.sh script to add an operating system user mob_app_usr inside a directory
+username="mob_app_usr"
+if getent passwd "$username" > /dev/null 2>&1; then
+    #echo "The username '$username' exits."
+else
+    #echo "The user '$username' does not exit. Lets create the user.
+    useradd mob_app_usr -m -d /home/mob_app_usr
+    #echo "User successfully created"
+fi
