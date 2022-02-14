7ad3c43f829ac03d454c29288310848fd7988560
JasraelAyinsakia <apuseyinejake011@gmail.com>
2022-02-14 20:56:02 +0000

    Clone a repository

---
+++ backend/sprint1/install.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+# Create script install.sh
+# This script should clone the repository into the home directory  of the mob_a>
+sudo mkdir mob_app_usr
+cd /home/mob_app_usr
+sudo git clone https://github.com/JasraelAyinsakia/Mobalysis.git /home/mob_app_usr
