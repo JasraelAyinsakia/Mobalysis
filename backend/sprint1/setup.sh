cc94db20d82e77e49f17ace8acc550bd2e5ffc41
bonganimokase <99387235+bonganimokase@users.noreply.github.com>
2022-02-17 10:28:09 +0200

    Update setup.sh

--- backend/sprint1/setup.sh
+++ backend/sprint1/setup.sh
@@ -2,9 +2,9 @@
 #Modify setup.sh script to add an operating system user mob_app_usr inside a directory
 username="mob_app_usr"
 if getent passwd "$username" > /dev/null 2>&1; then
-    #echo "The username does '$username' exits."
+    #echo "The username does '$username' exits"
 else
-    #echo "The user '$username' does not exit. Lets create the user.
+    #echo "The user '$username' does not exit.
     useradd mob_app_usr -m -d /home/mob_app_usr
     #echo "User successfully created"
 fi
